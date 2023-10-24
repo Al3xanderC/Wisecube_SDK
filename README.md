c0fa1273bed9b2b0f96c5f06a2e9bfecc5e65db4
Al3xanderC <cristian.paiusan00@gmail.com>
2023-10-24 12:27:46 +0300

    Created

---
+++ README.md
@@ -0,0 +1,1 @@
+Un sdk "schelet" pentru testare
---
+++ src/wisecube_sdk_skeleton_alexander2000/operations.py
@@ -0,0 +1,17 @@
+def suma(a, b):
+    return a + b
+
+
+def scadere(a, b):
+    return a - b
+
+
+def produs(a, b):
+    return a * b
+
+
+def imp(a, b):
+    if b == 0:
+        print("Valoarea lui b trebuie sa fie diferita de 0")
+    else:
+        return a / b
