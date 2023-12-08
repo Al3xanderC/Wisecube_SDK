6ef63a2c581a3a5cd1bc98ffb941ff5734a76ddd
pratacosmin <mcosmin1995@gmail.com>
2023-12-08 16:54:52 +0200

    try to fix install 8

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 import setuptools
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from pkg_resources import parse_requirements
+
+class CustomInstall(install):
+    def run(self):
+        with open('requirements.txt') as f:
+            requirements = [str(req) for req in parse_requirements(f.read())]
+
+        self.distribution.install_requires = requirements
+        install.run(self)
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
 
-
-setuptools.setup(
+setup(
     name="wisecube_sdk",
     version="1.0.6",
     author="Cosmin",
@@ -21,8 +30,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=required,
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.7",
+    cmdclass={'install': CustomInstall},
 )
