ffe14f9f198c076a371ed2fc06d213abaf99c185
pratacosmin <mcosmin1995@gmail.com>
2023-12-08 17:06:23 +0200

    try to fix install 9

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
-from setuptools import setup, find_packages
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+    required = f.read().splitlines()
 
-setup(
-    name='wisecube-sdk',
-    version='2.0.0',
-    packages=find_packages(),
-    install_requires=requirements,
+
+setuptools.setup(
+    name="scraper-package",
+    version="1.0.6",
+    author="Cosmin",
+    author_email="cosmin@wisecube.ai",
+    description="Databricks package version",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
