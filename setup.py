065f5d4c713eb51d8e31d959acbcb2cc50073d52
pratacosmin <mcosmin1995@gmail.com>
2023-12-08 17:01:39 +0200

    try to fix install 8

--- setup.py
+++ setup.py
@@ -1,27 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-with open("requirements.txt", "r") as f:
-    required = f.read().splitlines()
-
-
-setuptools.setup(
-    name="wisecube_sdk",
-    version="1.0.6",
-    author="Cosmin",
-    author_email="cosmin@wisecube.ai",
-    description="Wisecube SDK",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
+setup(
+    name='wisecube-sdk',
+    version='2.0.0',
+    packages=find_packages(),
+    install_requires=requirements,
 )
